bd620e6dc596f67a773388b3172446efc54a18a1
BenjaminMaring <benjammaring@gmail.com>
2024-01-29 16:37:09 -0700

    added more info displayed to the weather component and added styling for it

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import './styles/Weather.css'
 
 export default function Weather({ data }) {
+    console.log(data);
+
+    function getTime(unixTime, timezone) {
+        const date = new Date(unixTime * 1000 - timezone);
+
+        const options = { timeZone: 'your-time-zone-here' };
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+
+        hours = (hours < 10 ? "0" : "") + hours;
+        minutes = (minutes < 10 ? "0" : "") + minutes;
+        return hours + ":" + minutes;
+    }
     
     return(
             <div className="weather--wrapper">
@@ -17,9 +31,23 @@ export default function Weather({ data }) {
                 <div className="weather--limits">
                     <p>L  {data.main.temp_min.toFixed(1)} &nbsp;&nbsp; H {data.main.temp_max.toFixed(1)}</p>
                 </div>
+                <div className="weather--weather-desc">
+                    <p>{data.weather[0].description}</p>
+                </div>
+                <div className="weather--feels-like">
+                    <p>Feels like {data.main.feels_like}</p>
+                </div>
+                <div className="weather--wind-speed">
+                    <p>Wind {data.wind.speed}mph</p>
+                </div>
+                <div className="weather--humidity">
+                    <p>Humidity {data.main.humidity}%</p>
+                </div>
                 <div className="weather--sun">
-                    <p>sunrise {data.sys.sunrise}</p>
-                    <p>sunset {data.sys.sunrise}</p>
+                    <p>sunrise {getTime(data.sys.sunrise, data.timezone)}</p>
+                </div>
+                <div className="weather--sun">
+                    <p>sunset {getTime(data.sys.sunset, data.timezone)}</p>
                 </div>
             </div>
             
